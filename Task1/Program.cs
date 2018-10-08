bb0344f1da8ac72c458e693c28ad2f223778bbcd
endorph3ne <endorph3ne@gmail.com>
2018-10-08 22:55:12 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1 {
+    class Program {
+        static void Main(string[] args) {
+            string inputString = Console.ReadLine();
+            Console.WriteLine(CheckUniqueSymbols(inputString));
+            Console.Read();
+        }
+
+        static bool CheckUniqueSymbols(string inputString) {
+            HashSet<char> hashSet = new HashSet<char>();
+
+            for (int i = 0; i < inputString.Length; i++) {
+                if (hashSet.Contains(inputString[i]))
+                    return false;
+                else
+                    hashSet.Add(inputString[i]);
+            }
+
+            return true;
+        }
+    }
+}
