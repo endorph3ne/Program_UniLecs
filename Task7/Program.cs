6d73899dffc0421c9be1a3c5e9456abab7590ccc
endorph3ne <endorph3ne@gmail.com>
2018-11-29 23:22:13 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+/*
+    Задача: Дана строка, слова в ней указаны через пробел. Вывести слова в порядке убывания длины.
+*/
+
+/*
+    Идея: Сплит строки + OrderByDescending по длине слов
+*/
+
+namespace Task7 {
+    class Program {
+        static void Main(string[] args) {
+            string inputString = Console.ReadLine();
+            Console.WriteLine(GetSortedWordsByLong(inputString));
+            Console.Read();
+        }
+
+        static string GetSortedWordsByLong(string inputString) {
+            string[] stringArr = inputString.Split(' ');
+            stringArr = stringArr.OrderByDescending(x => x.Length).ToArray();
+            return string.Join(" ", stringArr);
+        }
+    }
+}
