10e69747ad28f7769211f5473c413899ad88da00
endorph3ne <endorph3ne@gmail.com>
2018-12-13 23:49:55 +0300

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+    Задача:  Поменять значения двух переменных без использования третьей.
+*/
+
+/*
+    Идея: Использовать математические операции и XOR
+*/
+
+namespace Task17 {
+    class Program {
+        static void Main(string[] args) {
+            int a = 3;
+            int b = 5;
+
+            SwapByXOR(ref a, ref b);
+
+            Console.Read();
+        }
+
+        static void SwapByMathOperations(ref int a, ref int b) {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+        }
+
+        static void SwapByXOR(ref int a, ref int b) {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+    }
+}
