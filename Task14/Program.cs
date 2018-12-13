c8d0720c945dc55a4b5f4c3ffd7280f97bacfd82
endorph3ne <endorph3ne@gmail.com>
2018-12-13 23:23:33 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+    Задача: вывод слов в строке в обратном порядке.Слова разделены только пробелами.
+*/
+
+/*
+    Идея: формируем слово, кладем его в стек, затем ковертируем стек в массив, а после в строку.
+*/
+
+namespace Task14 {
+    class Program {
+        static void Main(string[] args) {
+            string inputString = Console.ReadLine();
+            Console.WriteLine(RevertWordsInString(inputString));
+            Console.Read();
+        }
+
+        static string RevertWordsInString(string inputString) {
+            Stack<string> stackWords = new Stack<string>();
+            StringBuilder buildedWord = new StringBuilder();
+
+            for (int i = 0; i < inputString.Length; i++) {
+                char currentSymbol = inputString[i];
+
+                if (currentSymbol != ' ')
+                    buildedWord.Append(currentSymbol);
+                else {
+                    stackWords.Push(buildedWord.ToString());
+                    buildedWord.Clear();
+                }
+
+                if (i == inputString.Length - 1 && currentSymbol != ' ')
+                    stackWords.Push(buildedWord.ToString());
+            }
+
+            return String.Join(" ", stackWords.ToArray());
+        }
+    }
+}
